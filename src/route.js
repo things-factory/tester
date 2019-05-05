0e1aff66b09441c8c60a26861c8d48668a687870
Hearty Oh <heartyoh@hatiolab.com>
2019-05-05 22:12:19 +0800

    v0.1.11

--- src/route.js
+++ src/route.js
@@ -2,7 +2,7 @@ export default function route(page) {
   switch (page) {
     case 'index':
       /* menu-ui 모듈의 menu-list 페이지를 default page로 한다. */
-      return 'tester-menu-list'
+      return 'menu-list'
 
     case 'tester-menu-list':
       import('./pages/tester-menu-list')
