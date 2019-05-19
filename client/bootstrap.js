07627faad05ac73f2a31e313a104eb3feed8b151
Hearty Oh <heartyoh@hatiolab.com>
2019-05-19 18:09:38 +0900

    v0.1.14

--- client/bootstrap.js
+++ client/bootstrap.js
@@ -1,9 +1,9 @@
 import { store, UPDATE_BASE_URL } from '@things-factory/shell'
 
 export default function bootstrap() {
-  // store.dispatch({
-  // type: UPDATE_BASE_URL
-  // baseUrl: 'http://52.231.75.202/rest'
-  // baseUrl: 'http://board-demo.hatiolab.com/rest'
-  // })
+  store.dispatch({
+    type: UPDATE_BASE_URL,
+    // baseUrl: 'http://52.231.75.202/rest'
+    baseUrl: 'http://board-demo.hatiolab.com/rest'
+  })
 }
